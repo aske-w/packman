a36043c518f9a83c996035b83fb40157149432e4
alwe <alwe@itu.dk>
2022-02-21 11:59:43 +0100

    Added working strip algo canvas

--- src/components/games/stripPacking/StripAlgoCanvas.tsx
+++ src/components/games/stripPacking/StripAlgoCanvas.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useImperativeHandle, useState } from "react";
-import { Layer, Rect, Stage } from "react-konva";
+import React, { useEffect, useImperativeHandle, useRef, useState } from "react";
+import { KonvaNodeEvents, Layer, Rect, Stage } from "react-konva";
 import { BestFitDecreasingHeight } from "../../../algorithms/BestFitDecreasingHeight";
 import { FirstFitDecreasingHeight } from "../../../algorithms/FirstFitDecreasingHeight";
 import { NextFitDecreasingHeight } from "../../../algorithms/NextFitDecreasingHeight";
-import { SizeAlternatingStack } from "../../../algorithms/SizeAlternatingStack";
 import {
   CanvasProps,
+  GAME_HEIGHT,
   INVENTORY_SIZE,
   PADDING,
   SCROLLABLE_HEIGHT,
@@ -20,6 +20,8 @@ import {
 } from "../../../types/PackingAlgorithm.interface";
 import { DimensionsWithConfig } from "../../../types/DimensionsWithConfig.type";
 import { RectangleConfig } from "../../../types/RectangleConfig.interface";
+import Konva from "konva";
+import { Rect as KonvaRect, RectConfig } from "konva/lib/shapes/Rect";
 
 const {
   BEST_FIT_DECREASING_HEIGHT,
@@ -32,48 +34,58 @@ interface StripAlgoCanvasProps extends CanvasProps {
   algorithm: PackingAlgorithms;
 }
 
-interface StripAlgoCanvasHandle {
+export interface StripAlgoCanvasHandle {
   place(): void;
 }
+
+type PrevPos = { prevX: number; prevY: number };
 
 const StripAlgoCanvas = React.forwardRef<
   StripAlgoCanvasHandle,
   StripAlgoCanvasProps
 >(({ input, algorithm }, ref) => {
-  const [stripRects, setStripRects] = useState<ColorRect<{}>[]>([]);
+  const [stripRects, setStripRects] = useState<
+    ColorRect<RectangleConfig & PrevPos>[]
+  >([]);
   const [inventoryRects, setInventoryRects] = useState<
-    DimensionsWithConfig<RectangleConfig>[]
+    ColorRect<RectangleConfig>[]
   >([]);
 
-  const [algo, setAlgo] = useState<PackingAlgorithm | null>(null);
+  const [algo, setAlgo] = useState<PackingAlgorithm<RectangleConfig> | null>(
+    null
+  );
 
-  const calcInitialPositions = (rects: DimensionsWithConfig[]) => {
-    return rects.map<ColorRect<{}>>((attrs, i, arr) => {
-      const { height } = attrs;
-      const x = INVENTORY_SIZE.width / 2 + PADDING;
+  const calcInitialPositions = (
+    rects: DimensionsWithConfig<RectangleConfig>[]
+  ) => {
+    return rects.reduce<ColorRect<RectangleConfig>[]>((acc, attrs, i) => {
+      const { height, width } = attrs;
+      const x = INVENTORY_SIZE.width / 2 - width / 2;
       if (i === 0) {
-        return {
+        acc.push({
           ...attrs,
           x,
           y: -height - PADDING,
-        };
+        });
       } else {
-        const prev = (arr as ColorRect[])[i - 1];
-        return {
+        const prev = acc[i - 1];
+        acc.push({
           ...attrs,
           x,
           y: prev.y - height - PADDING,
-        };
+        });
       }
-    });
+
+      return acc;
+    }, []);
   };
 
   useEffect(() => {
     const getAlgo = (algorithm: PackingAlgorithms) => {
-      const size = { ...STAGE_SIZE };
+      const size = { ...STRIP_SIZE };
       switch (algorithm) {
         case NEXT_FIT_DECREASING_HEIGHT:
-          const a = new NextFitDecreasingHeight(size).load(inventoryRects);
+          const a = new NextFitDecreasingHeight(size).load(input);
           // .getSortedData();
           return a;
           break;
@@ -95,14 +107,17 @@ const StripAlgoCanvas = React.forwardRef<
 
     const algo = getAlgo(algorithm);
     setAlgo(algo);
-    setInventoryRects(algo.getSortedData());
+    setInventoryRects(calcInitialPositions(algo.getSortedData()));
   }, [algorithm]);
 
   useImperativeHandle(ref, () => ({
     place: () => {
       const rect = algo?.place();
       if (rect) {
-        setStripRects((prev) => [...prev, rect]);
+        const { x: prevX, y: prevY } = inventoryRects.find(
+          (r) => r.name === rect.name
+        )!;
+        setStripRects((prev) => [...prev, { ...rect, prevX, prevY }]);
       }
     },
   }));
@@ -118,6 +133,31 @@ const StripAlgoCanvas = React.forwardRef<
           <Layer>
             <Rect fill="#ffffff" {...STRIP_SIZE} />
             <Rect fill="#eee000" {...INVENTORY_SIZE} />
+            {inventoryRects.map((r) => {
+              return (
+                <Rect
+                  key={r.name}
+                  {...r}
+                  strokeWidth={2}
+                  stroke={"#002050FF"}
+                  y={GAME_HEIGHT + r.y}
+                  id={`INVENTORY_RECT`}
+                />
+              );
+            })}
+            {stripRects.map((r) => {
+              return (
+                <MyRect
+                  key={r.name}
+                  {...r}
+                  x={r.x + INVENTORY_SIZE.width}
+                  strokeWidth={2}
+                  stroke={"#002050FF"}
+                  y={GAME_HEIGHT + r.y}
+                  id={`STRIP_RECT`}
+                />
+              );
+            })}
           </Layer>
         </Stage>
       </div>
@@ -126,6 +166,38 @@ const StripAlgoCanvas = React.forwardRef<
 });
 
 export default StripAlgoCanvas;
+
+const MyRect: React.FC<PrevPos & RectConfig & KonvaNodeEvents> = ({
+  x,
+  y,
+  prevX,
+  prevY,
+  ...props
+}) => {
+  const ref = useRef<KonvaRect>(null);
+  useEffect(() => {
+    new Konva.Tween({
+      node: ref.current!,
+      duration: 1.4,
+      x,
+      y,
+      easing: Konva.Easings.StrongEaseInOut,
+      rotation: 360,
+    }).play();
+  }, [x, y]);
+
+  return (
+    <Rect
+      ref={ref}
+      x={prevX}
+      y={prevY + GAME_HEIGHT}
+      stroke={"rgba(0,0,0,0.2)"}
+      strokeWidth={1}
+      draggable
+      {...props}
+    ></Rect>
+  );
+};
 
 /*
           <Layer {...STRIP_SIZE}>
--- src/pages/games/StripPackingGame.tsx
+++ src/pages/games/StripPackingGame.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useState } from "react";
-import StripAlgoCanvas from "../../components/games/stripPacking/StripAlgoCanvas";
+import React, { useCallback, useRef, useState } from "react";
+import StripAlgoCanvas, {
+  StripAlgoCanvasHandle,
+} from "../../components/games/stripPacking/StripAlgoCanvas";
 import StripPacking from "../../components/games/stripPacking/StripPacking";
 import { genInventory } from "../../config/canvasConfig";
 import { ColorRect } from "../../types/ColorRect.interface";
@@ -9,19 +11,30 @@ interface StripPackingGameProps {}
 
 const StripPackingGame: React.FC<StripPackingGameProps> = ({}) => {
   const [input, setInput] = useState(genInventory);
+  const ref = useRef<StripAlgoCanvasHandle>(null);
   //   state gen data
 
-  const onStripDrop = useCallback((rect: ColorRect) => {}, []);
-
-  const genData = () => {};
+  const onStripDrop = () => {
+    ref?.current?.place();
+  };
 
   return (
-    <div className="w-full flex justify-between items-center">
-      <StripPacking input={input}></StripPacking>
-      <StripAlgoCanvas
-        input={input}
-        algorithm={PackingAlgorithms.NEXT_FIT_DECREASING_HEIGHT}
-      ></StripAlgoCanvas>
+    <div className="w-full">
+      <button
+        onClick={onStripDrop}
+        className="bg-red-500 text-white px-2 py-2 text-xl"
+      >
+        Place
+      </button>
+
+      <div className="w-full flex justify-between items-center">
+        <StripPacking input={input}></StripPacking>
+        <StripAlgoCanvas
+          ref={ref}
+          input={input}
+          algorithm={PackingAlgorithms.NEXT_FIT_DECREASING_HEIGHT}
+        ></StripAlgoCanvas>
+      </div>
     </div>
   );
 };
