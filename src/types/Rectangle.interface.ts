8fde718d1f99ef8237139f001ef99bf854029bee
askw <askw@itu.dk>
2022-03-07 10:28:01 +0100

    refactor rectangle interface

--- src/types/Rectangle.interface.ts
+++ src/types/Rectangle.interface.ts
@@ -1,6 +1,6 @@
 import { Dimensions } from './Dimensions.interface';
+import { Coordinate } from './Coordinate.interface'
 
-export interface Rectangle extends Dimensions {
-  x: number;
-  y: number;
+export interface Rectangle extends Dimensions, Coordinate {
+  
 }
