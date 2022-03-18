4d49845b5b861d7cf23c47d4387dfc414268ce0b
Jacob Mølby <jacob.moelby@gmail.com>
2022-03-18 10:15:19 +0100

    NFDH and FFDH articles

--- src/components/playground/strip/algorithm-articles/FFDHArticle.tsx
+++ src/components/playground/strip/algorithm-articles/FFDHArticle.tsx
@@ -1,13 +1,8 @@
-import Konva from 'konva';
-import { Rect as KonvaRect, RectConfig } from 'konva/lib/shapes/Rect';
-import { IRect } from 'konva/lib/types';
-import { nanoid } from 'nanoid';
-import React, { useEffect, useRef, useState } from 'react';
-import { Layer, Rect, Stage } from 'react-konva';
+import React, { useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
 
-interface NFDHArticleProps {}
-const FFDHArticle: React.FC<NFDHArticleProps> = ({}) => {
+interface FFDHArticleProps {}
+const FFDHArticle: React.FC<FFDHArticleProps> = ({}) => {
   useEffect(() => {
     ReactTooltip.rebuild();
   });
@@ -31,34 +26,38 @@ const FFDHArticle: React.FC<NFDHArticleProps> = ({}) => {
       </p>
       <div className="flex flex-col items-center p-5 bg-gray-700 rounded-lg not-prose">
         <svg
-          width="797"
+          width="792"
           height="158"
-          viewBox="0 0 797 158"
+          viewBox="0 0 792 158"
           fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <rect y="126" width="150" height="32" fill="#FBBF24" />
-          <rect x="196" y="59" width="78" height="99" fill="#FBBF24" />
-          <rect x="320" y="30" width="78" height="128" fill="#FBBF24" />
-          <rect x="444" y="109" width="78" height="49" fill="#FBBF24" />
-          <rect x="568" y="69" width="26" height="89" fill="#FBBF24" />
-          <rect x="640" width="157" height="158" fill="#FBBF24" />
+          <rect x="170" y="59" width="78" height="99" fill="#FBBF24" />
+          <rect x="268" y="30" width="78" height="128" fill="#FBBF24" />
+          <rect x="366" y="109" width="78" height="49" fill="#FBBF24" />
+          <rect x="464" width="157" height="158" fill="#FBBF24" />
+          <rect x="641" y="139" width="105" height="19" fill="#FBBF24" />
+          <rect x="766" y="69" width="26" height="89" fill="#FBBF24" />
         </svg>
+
         <p className="mt-5 text-sm text-white">Unsorted input</p>
       </div>
       <div className="flex flex-col items-center p-5 mt-10 bg-gray-700 rounded-lg not-prose">
         <svg
-          width="797"
+          width="784"
           height="158"
-          viewBox="0 0 797 158"
+          viewBox="0 0 784 158"
           fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <rect width="157" height="158" fill="#FBBF24" />
-          <rect x="203" y="30" width="78" height="128" fill="#FBBF24" />
-          <rect x="327" y="59" width="78" height="99" fill="#FBBF24" />
-          <rect x="451" y="69" width="26" height="89" fill="#FBBF24" />
-          <rect x="523" y="109" width="78" height="49" fill="#FBBF24" />
-          <rect x="647" y="126" width="150" height="32" fill="#FBBF24" />
+          <rect x="175.667" y="30" width="78" height="128" fill="#FBBF24" />
+          <rect x="272.333" y="59" width="78" height="99" fill="#FBBF24" />
+          <rect x="369" y="69" width="26" height="89" fill="#FBBF24" />
+          <rect x="413.667" y="109" width="78" height="49" fill="#FBBF24" />
+          <rect x="510.333" y="126" width="150" height="32" fill="#FBBF24" />
+          <rect x="679" y="139" width="105" height="19" fill="#FBBF24" />
         </svg>
+
         <p className="mt-5 text-sm text-white">Sorted input</p>
       </div>
       <h4>Execution</h4>
@@ -68,20 +67,262 @@ const FFDHArticle: React.FC<NFDHArticleProps> = ({}) => {
         new shelf with the height of the first placed rectangle.
       </p>
       <p>
-        When placing the next rectangle, we now look at the current shelf. We
-        check that the remaining width of the shelf is enough to accomodate the
-        rectangle. This leads to two cases:
+        When placing the next rectangle, we now look at the bottom most shelf.
+        We check that the remaining width of the shelf is enough to accomodate
+        the rectangle. This leads to two cases:
       </p>
       <ol>
         <li>
           If there is enough space, the rectangle is placed in the same shelf
         </li>
         <li>
-          Otherwise, we create a new shelf above the current one and place the
-          rectangle there
+          Otherwise, we check the shelf above to see if it can accomodate the
+          rectangle. If it can it will be placed. Otherwise we continue checking
+          all shelves above, if none can accomodate it, we create a new shelf
+          and it is placed there.
         </li>
       </ol>
       <p>This is repeated until the entire input is packed</p>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center p-5 bg-gray-700 rounded-lg not-prose max-w-min">
+          <svg
+            width="563"
+            height="319"
+            viewBox="0 0 563 319"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect width="554" height="319" fill="white" />
+            <rect
+              x="231.5"
+              y="220.5"
+              width="77"
+              height="98"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="309.5"
+              y="230.5"
+              width="25"
+              height="88"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="335.5"
+              y="270.5"
+              width="77"
+              height="48"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="413.5"
+              y="287.5"
+              width="149"
+              height="31"
+              fill="#FBBF24"
+              fill-opacity="0.5"
+              stroke="black"
+            />
+            <rect
+              x="156.5"
+              y="191.5"
+              width="77"
+              height="127"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="0.5"
+              y="161.5"
+              width="156"
+              height="157"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <line
+              x1="2"
+              y1="160"
+              x2="554"
+              y2="160"
+              stroke="black"
+              stroke-width="2"
+              stroke-dasharray="4 4"
+            />
+          </svg>
+          <p className="mt-5 text-sm text-white">
+            Rectangle cannot fit on the first shelf.
+          </p>
+        </div>
+        <div className="flex flex-col items-center self-center p-5 mt-5 bg-gray-700 rounded-lg not-prose max-w-min">
+          <svg
+            width="554"
+            height="319"
+            viewBox="0 0 554 319"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect width="554" height="319" fill="white" />
+            <rect
+              x="231.5"
+              y="220.5"
+              width="77"
+              height="98"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="309.5"
+              y="230.5"
+              width="25"
+              height="88"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="335.5"
+              y="270.5"
+              width="77"
+              height="48"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="0.5"
+              y="129.5"
+              width="149"
+              height="31"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="156.5"
+              y="191.5"
+              width="77"
+              height="127"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="0.5"
+              y="161.5"
+              width="156"
+              height="157"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <line
+              x1="2"
+              y1="160"
+              x2="554"
+              y2="160"
+              stroke="black"
+              stroke-width="2"
+              stroke-dasharray="4 4"
+            />
+            <line
+              x1="2"
+              y1="128"
+              x2="554"
+              y2="128"
+              stroke="black"
+              stroke-width="2"
+              stroke-dasharray="4 4"
+            />
+          </svg>
+          <p className="mt-5 text-sm text-white">
+            A new shelf is created to fit the rectangle.
+          </p>
+        </div>
+        <div className="flex flex-col items-center self-center p-5 mt-5 bg-gray-700 rounded-lg not-prose max-w-min">
+          <svg
+            width="554"
+            height="319"
+            viewBox="0 0 554 319"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect width="554" height="319" fill="white" />
+            <rect
+              x="413.5"
+              y="300.5"
+              width="104"
+              height="18"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="231.5"
+              y="220.5"
+              width="77"
+              height="98"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="309.5"
+              y="230.5"
+              width="25"
+              height="88"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="335.5"
+              y="270.5"
+              width="77"
+              height="48"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="0.5"
+              y="129.5"
+              width="149"
+              height="31"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="156.5"
+              y="191.5"
+              width="77"
+              height="127"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <rect
+              x="0.5"
+              y="161.5"
+              width="156"
+              height="157"
+              fill="#FBBF24"
+              stroke="black"
+            />
+            <line
+              x1="2"
+              y1="160"
+              x2="554"
+              y2="160"
+              stroke="black"
+              stroke-width="2"
+              stroke-dasharray="4 4"
+            />
+            <line
+              x1="2"
+              y1="128"
+              x2="554"
+              y2="128"
+              stroke="black"
+              stroke-width="2"
+              stroke-dasharray="4 4"
+            />
+          </svg>
+
+          <p className="mt-5 text-sm text-white">
+            The last rectangle fits in the bottom shelf.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
