65bf5b43730e001c614c5ca4a38ac29afee6bc19
askw <askw@itu.dk>
2022-03-28 12:13:01 +0200

    tracking implemented for current progress

--- src/types/Gamemodes.enum.ts
+++ src/types/Gamemodes.enum.ts
@@ -2,3 +2,8 @@ export enum Gamemodes {
   BIN_PACKING = 'Bin Packing',
   STRIP_PACKING = 'Strip Packing',
 }
+
+export const ALL_GAMEMODES = [
+  Gamemodes.BIN_PACKING,
+  Gamemodes.STRIP_PACKING
+]
