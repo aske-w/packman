41d64e67691aee5a541fe16884d44403790f7400
Jacob Mølby <jacob.moelby@gmail.com>
2022-04-08 17:02:48 +0200

    animated logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { pathName } from './routes';
 import Logo from '../../src/resources/Logo.svg';
+import { Layer, Rect, Stage } from 'react-konva';
+import { Rect as KonvaRect } from 'konva/lib/shapes/Rect';
+import Konva from 'konva';
+import chunk from 'lodash/chunk';
 interface HomeProps {}
+
+const DELAY = 200;
+const logoRects = [
+  { x: 0, y: 28.4337, width: 11.2, height: 11.5663, fill: '#34D399' },
+  { x: 1, y: 29.4337, width: 9.2, height: 9.56627, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 11.2, y: 28.4337, width: 20, height: 11.5663, fill: '#FBBF24' },
+  { x: 12.2, y: 29.4337, width: 18, height: 9.56627, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { y: 14.2169, width: 9.6, height: 14.2169, fill: '#FB7185' },
+  { x: 1, y: 15.2169, width: 7.6, height: 12.2169, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 9.59998, y: 14.2169, width: 18, height: 14.2169, fill: '#60A5FA' },
+  { x: 10.6, y: 15.2169, width: 16, height: 12.2169, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 27.6, y: 14.2169, width: 9.4, height: 14.2169, fill: '#C084FC' },
+  { x: 28.6, y: 15.2169, width: 7.4, height: 12.2169, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 30.4, y: 0, width: 9.6, height: 14.2169, fill: '#FB923C' },
+  { x: 31.4, y: 1, width: 7.6, height: 12.2169, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 31.2, y: 28.4337, width: 8.8, height: 11.5663, fill: '#C084FC' },
+  { x: 32.2, y: 29.4337, width: 6.8, height: 9.56627, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+  { x: 37, y: 14.2169, width: 3, height: 14.2169, fill: '#22D3EE' },
+  { x: 37.75, y: 14.9669, width: 1.5, height: 12.7169, stroke: 'black', opacity: 0.49, strokeWidth: 1.5 },
+  { x: 0, y: 0, width: 30.4, height: 14.2169, fill: '#F472B6' },
+  { x: 1, y: 1, width: 28.4, height: 12.2169, stroke: 'black', opacity: 0.49, strokeWidth: 2 },
+];
+const FallingRect = ({ y, i, ...attrs }: typeof logoRects[number] & { i: number }) => {
+  const ref = useRef<KonvaRect>(null);
+  useEffect(() => {
+    const tid = setTimeout(() => {
+      new Konva.Tween({
+        node: ref.current!,
+        duration: 0.4,
+        y,
+        easing: Konva.Easings.StrongEaseInOut,
+      }).play();
+    }, i * DELAY);
+    return () => clearTimeout(tid);
+  }, [y, i]);
+  return <Rect ref={ref} listening={false} {...attrs} y={y - 100} />;
+};
+
+const AnimatedLogo = () => {
+  return (
+    <Stage width={80} height={80} scale={{ x: 2, y: 2 }} className="w-20 h-20">
+      <Layer>
+        {chunk(logoRects, 2).map(([r, s], i) => (
+          <React.Fragment key={i}>
+            <FallingRect i={i} {...r} />
+            <FallingRect i={i} {...s} />
+          </React.Fragment>
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
 
 const Home: React.FC<HomeProps> = ({}) => {
   return (
     <div className="w-screen p-10 bg-gradient-to-b from-gray-700 to-gray-800 backdrop-blur backdrop-filter">
       <div className="flex flex-col items-center justify-start w-full">
         <div className="flex items-center space-x-4">
-          <img src={Logo} alt="logo" className="w-20 h-20" />
+          <AnimatedLogo />
+          {/* <img src={Logo} alt="logo" className="w-20 h-20" /> */}
           <h1 className="text-6xl font-extrabold tracking-wide text-white">
             Packman <span className="text-sm font-normal tracking-normal">by Learnpacking.com</span>
           </h1>
