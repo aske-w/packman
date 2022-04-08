93f2cbd40817e4a7ac0ed1a99048fb5886ce65f2
alwe <alwe@itu.dk>
2022-04-08 11:46:17 +0200

    Removed unused file

--- src/pages/games/OnlineBinPackingGame.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface OnlineBinPackingGameProps {}
-
-const OnlineBinPackingGame: React.FC<OnlineBinPackingGameProps> = ({}) => {
-  return <div>ONLINE BIN PACKING GAME</div>;
-};
-
-export default OnlineBinPackingGame;
