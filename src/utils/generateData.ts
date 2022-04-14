e7aa9555b8d18920c6fb24e55b9299b064a04a2e
askw <askw@itu.dk>
2022-04-14 12:53:04 +0200

    generate inventory from array of dimensions

--- src/utils/generateData.ts
+++ src/utils/generateData.ts
@@ -18,6 +18,46 @@ export const generateData = (amount = 10, max = 100, min = 50): Dimensions[] => 
   });
 };
 
+export const generateInventoryFromDimensions = <T = RectangleConfig>(dims: Dimensions[]): ColorRect<T>[]  => {
+  return dims.reduce<Acc<T>>(
+    (acc, attrs, i) => {
+      const { height, width } = attrs;
+
+      acc.row.prevRowHeight = Math.max(acc.row.y + height, acc.row.prevRowHeight);
+
+      const rect: any = {
+        width,
+        height,
+        x: PADDING,
+        y: PADDING,
+        fill: Konva.Util.getRandomColor(),
+        name: genId(),
+      };
+      if (i === 0) {
+        acc.rects.push(rect);
+      } else {
+        const prev = acc.rects[i - 1];
+        const x = PADDING + prev.x + prev.width;
+        if (x + width >= dims.length) {
+          // should create new row
+          acc.row.y = acc.row.prevRowHeight + PADDING;
+          rect.y = acc.row.prevRowHeight + PADDING * 2;
+          rect.x = PADDING;
+        } else {
+          // continue in row
+          rect.y = acc.row.y + PADDING;
+          rect.x = x;
+        }
+
+        acc.rects.push(rect);
+      }
+
+      return acc;
+    },
+    { rects: [], row: { prevRowHeight: 0, y: 0 } }
+  ).rects;
+};
+
 export const generateInventory = <T = RectangleConfig>(inventorySize: number, numItems = 50) => {
   return generateData(numItems, 200, 25).reduce<Acc<T>>(
     (acc, attrs, i) => {
